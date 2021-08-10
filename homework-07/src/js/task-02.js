3654a9e4429330f2eb2887a31d976f3a1109132c
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2021-08-10 14:14:47 +0300

    Задача 2

--- homework-07/src/js/task-02.js
+++ homework-07/src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ulIngredients = ingredients.map(elem => {
+  const ulIngredients = document.createElement('li');
+  ulIngredients.textContent = elem;
+  return ulIngredients;
+});
+
+const listIngredients = document.querySelector('#ingredients');
+
+listIngredients.append(...ulIngredients);
+
+console.log(listIngredients);
+
